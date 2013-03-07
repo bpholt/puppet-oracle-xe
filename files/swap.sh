0b07c80462d703a6bfd031d6ac45f94196e60f50
Brian P. Holt <bholt@planetholt.com>
2013-03-07 15:07:57 -0600

    adds script to add swap space if needed before oracle install

--- files/swap.sh
+++ files/swap.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+if [ ! -e /proc/meminfo ]
+then
+  exit 42
+fi
+
 SWAP=`grep SwapTotal /proc/meminfo | sed 's/[^0-9]//g'`
 TOTAL=`grep MemTotal /proc/meminfo | sed 's/[^0-9]//g'`
 
-GLOBAL_MIN_SWAP=2097152
-RAM_MIN_SWAP=$[TOTAL*2]
+BUFFER=5000
+
+GLOBAL_MIN_SWAP=$[2097152+BUFFER]
+RAM_MIN_SWAP=$[TOTAL*2+BUFFER]
+
+SWAP_TO_CREATE="none"
 
 if [ "$SWAP" -lt "$GLOBAL_MIN_SWAP" ]
 then
@@ -12,9 +21,20 @@ then
   then
     if [ "$RAM_MIN_SWAP" -lt "$GLOBAL_MIN_SWAP" ]
     then
-      echo "make $RAM_MIN_SWAP swap"
+      SWAP_TO_CREATE=$[RAM_MIN_SWAP-SWAP]
     else
-      echo "make 2GB swap"
+      SWAP_TO_CREATE=$[GLOBAL_MIN_SWAP-SWAP]
     fi
   fi
+fi
+
+if [ "$SWAP" == "none" ]
+then
+  exit 0
+else
+  TMP=`mktemp`
+
+  dd if=/dev/zero of=$TMP bs=1024 count=$SWAP_TO_CREATE
+  mkswap $TMP
+  swapon -a $TMP
 fi
--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,7 @@ class oracle-xe (
 
   $oracle_rpm = "oracle-xe-11.2.0-1.0.x86_64.rpm"
   $oracle_rpm_tmp = "/tmp/$oracle_rpm"
+  $oracle_swap_script = "/u01/app/oracle/local/swap.sh"
 
   file { 'oracle-xe-rpm':
     path   => "$oracle_rpm_tmp",
@@ -17,6 +18,27 @@ class oracle-xe (
     mode   => 0444,
     owner  => root,
     group  => root,
+  }
+
+  file { 'oracle-local':
+    ensure => directory,
+    path   => [ '/u01', '/u01/app', '/u01/app/oracle', '/u01/app/oracle/local'],
+    before => File['oracle-swap'],
+  }
+
+  file { 'oracle-swap':
+    path   => "$oracle_swap_script",
+    ensure => file,
+    source => "puppet:///modules/oracle-xe/swap.sh",
+    mode   => 0544,
+    owner  => root,
+    group  => root,
+    before => Exec['oracle-swap'],
+  }
+
+  exec { 'oracle-swap':
+    command => "$oracle_swap_script",
+    before  => Package['oracle-xe'],
   }
 
   # only necessary because we don't use yum to install the main package
